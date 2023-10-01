a90ce01ee0eee3d8a329860ed532680d4bf4628b
Vinayak Singh <vinayaksingh10142@gmail.com>
2023-10-01 22:10:55 +0530

    image updated

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ const About = () => {
         <div className="left_part">
           <div className="img">
             <div className="img_in" data-bg-img="/img/4.jpg">
-              {/* <img src="/img/1x1.jpg" alt="" /> */}
+              <img src="/img/1x1.jpg" alt="" />
               {/* <Image src="/img/1x1.jpg" alt="" width={width} height={height} /> */}
 
 
