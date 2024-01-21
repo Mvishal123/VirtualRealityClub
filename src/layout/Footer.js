5dbd2bf539875fab8a82e75ab322e6a4efeb3a8b
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-01-22 01:11:47 +0530

    removed copyright

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -6,35 +6,9 @@ const Footer = () => {
         <div className="footer">
           <div className="left_part">
             <p>
-              Copyright 2023 — Designed &amp; Developed by{" "}
-              <a
-                href="https://www.vinayaksingh.com"
-                target="_blank"
-                rel="noreferrer"
-              >
-                vinayakSingh
-              </a>
+              Copyright 2024 - VR CLUB VIT Chennai
             </p>
-          </div>
-          <div className="right_part">
-            <ul>
-              <li>
-                <Link href="">
-                  <a className="creative_link">Privacy Policy</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <a className="creative_link">Cookies</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="">
-                  <a className="creative_link">Terms &amp; Conditions</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
+          </div>  
         </div>
       </div>
     </footer>
