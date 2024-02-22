555b9c2b595ff9c2c4dfd2aa4aad02fefaf0f8ee
Vinayak Singh <vinayaksingh10142@gmail.com>
2024-02-22 21:35:36 +0530

    coming soon page updated

--- pages/coming-soon.js
+++ pages/coming-soon.js
@@ -1,54 +1,66 @@
 import Layout from "../src/layout/Layout";
-const ComingSoon = () => {
+import { useEffect, useState } from "react";
+
+const ComingSoon = ({ days, hours, minutes, seconds }) => {
+  // State to hold the time remaining
+  const [timeRemaining, setTimeRemaining] = useState({ days, hours, minutes, seconds });
+
+  // Update the time remaining every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining((prevTime) => {
+        const newTime = calculateTimeRemaining("Februrary 25, 2024 12:30:00");
+        return { ...prevTime, ...newTime };
+      });
+    }, 1000);
+
+    // Clear the interval when the component is unmounted
+    return () => clearInterval(timer);
+  }, []);
+
+  // Function to calculate the time remaining until the specified date
+  const calculateTimeRemaining = (dueDate) => {
+    const now = new Date();
+    const endDate = new Date(dueDate);
+    const timeRemaining = endDate - now;
+
+    let days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+    return { days, hours, minutes, seconds };
+  };
+
   return (
     <Layout pageTitle={"Coming Soon"}>
       <div className="metaportal_fn_coming_soon">
         <div className="container">
-          <div className="soon_countdown">
-            {/* 
-						There is two types of countdown: due_date (Due Date), ever (Evergreen timer)
-							1. 	data-type="due_date"
-								In this case you have to change value of data-date. For example:
-								data-date="October 13, 2022 12:30:00"
-								It will mean that mint will finished at this time
-
-							2. 	data-type="ever"
-								In this case you have to change values of data-days, data-hours, data-minutes and data-seconds. For example:
-								data-days="34"
-								data-hours="10"
-								data-minutes="20"
-								data-seconds="0"
-								It will mean that the time expires after this time, but when the page is refreshed, the value will return again. It means, it won't end.
-						Add boxed class to get #1 type of countdown
-					*/}
-            <h3
-              className="metaportal_fn_countdown boxed"
-              data-type="ever"
-              data-date="October 13, 2022 12:30:00"
-              data-days={34}
-              data-hours={9}
-              data-minutes={20}
-              data-seconds={10}
-            >
-              0d: 0h: 0m: 0s
+          <div className="soon_countdown center">
+            {/* Display the dynamically calculated time remaining */}
+            <h3 className="metaportal_fn_countdown boxed">
+              {`${timeRemaining.days}d: ${timeRemaining.hours}h: ${timeRemaining.minutes}m: ${timeRemaining.seconds}s`}
             </h3>
           </div>
           <div className="soon_title">
-            <h3
-              className="fn__maintitle"
-              data-text="Public Minting is Coming Soon"
-              data-align="center"
-            >
-              Public Minting is Coming Soon
+            <h3 className="fn__maintitle" data-text="Coming Soon" data-align="center">
+              Coming Soon
             </h3>
-            <p>
-              Our website is coming soon. We are currently working on our
-              website. Please check again within couple days.
-            </p>
+            <p>We are currently working on our website.</p>
           </div>
         </div>
       </div>
+
+      <style jsx>{`
+        .center {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          text-align: center;
+        }
+      `}</style>
     </Layout>
   );
 };
+
 export default ComingSoon;
